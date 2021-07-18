4a90d29adf2dceebb7f475d2abb938229b78ebe6
Julien Guy <jguy@lbl.gov>
2021-07-18 12:06:02 -0700

    more tries

--- check-red-leg-metrology.py
+++ check-red-leg-metrology.py
@@ -21,10 +21,10 @@ def main() :
         ifilename = sys.argv[1]
     else :
         print("""
-Please add as an argument a filename.
-I will use a default file for now as a code test.
+Please add as an argument a filename:
+test-petal6-light-red-leg.yaml or test-petal6-heavy-red-leg.yaml
         """)
-        ifilename = "test-petal0.yaml"
+        sys.exit(12)
 
     if not os.path.isfile(ifilename) :
         print("cannot find or open",ifilename)
@@ -61,11 +61,14 @@ I will use a default file for now as a code test.
     measured_bmr_CS5_inch = np.zeros((3,number_of_balls))
 
     print("Input BMR coordinates (inch):")
-    valid_bmr = np.repeat(False,number_of_balls)
+    valid_bmr = np.repeat(True,number_of_balls)
     for index,bmr_label in enumerate(bmr_labels) :
         if bmr_label in inputs :
             measured_bmr_CS5_inch[:,index] = str2array(inputs[bmr_label])
             print("{} {}".format(bmr_label,inputs[bmr_label]))
+
+    # align target bmr in z ?
+    # target_bmr_CS5_inch[2] = np.mean(target_bmr_CS5_inch[2])
 
     xyz1=target_bmr_CS5_inch[:,valid_bmr]*inch2mm+0.
     xyz2=measured_bmr_CS5_inch[:,valid_bmr]*inch2mm+0.
@@ -74,10 +77,15 @@ I will use a default file for now as a code test.
     print("measured-target BMR dx (mm)=",np.mean(xyz2[0]-xyz1[0]))
     print("measured-target BMR dy (mm)=",np.mean(xyz2[1]-xyz1[1]))
     xyz2[2] += np.mean(xyz1[2]-xyz2[2])
-    xyz2[2] = xyz1[2]
+
+    # just ignore the offsets in z?
+    #xyz2[2] = xyz1[2]
+
     cs5_invert_mm = Transfo()
     rms=cs5_invert_mm.fit(xyz1,xyz2)
-    print(" pm_invert fit rms=",rms,"mm")
+    print("Inverse transfo fit rms=",rms,"mm")
+    print(cs5_invert_mm)
+
     xyz = get_red_leg_mount_holes_in_CS5_mm(petal)
     xyz2 = cs5_invert_mm.apply(xyz)
     for b in range(xyz.shape[1]) :
@@ -91,7 +99,7 @@ I will use a default file for now as a code test.
     xyz3[:,1]=[-270.14129446,310.56739657,-1759.27470220]
     xyz3[:,2]=[-219.92149856,364.62873842,-1759.1524087]
     for b in range(xyz3.shape[1]) :
-        print("solidworks , hole",b,"xyz CS5 mm =",array2str(xyz3[:,b]))
+        print("solidworks, hole",b,"xyz CS5 mm =",array2str(xyz3[:,b]))
     for b in range(xyz3.shape[1]) :
         print("delta, hole",b,"xyz CS5 mm =",array2str(xyz2[:,b]-xyz3[:,b]))
 
