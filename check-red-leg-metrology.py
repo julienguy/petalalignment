67798c4547ed8dd5c3183c09a28e7bd2d0fedb24
Julien Guy <jguy@lbl.gov>
2021-07-18 18:36:56 -0700

    read yaml files

--- check-red-leg-metrology.py
+++ check-red-leg-metrology.py
@@ -7,9 +7,11 @@ import scipy.optimize as opt
 import matplotlib.pyplot as plt
 import yaml
 
-from transfo import Transfo3D as Transfo
-from PAC import array2str,str2array
-from PAC import compute_target_bmr_light_weight_red_leg_coords_mm,get_red_leg_mount_holes_in_CS5_mm
+from transfo import Transfo3D,Transfo2D
+from PAC import array2str,str2array,\
+    compute_target_bmr_light_weight_red_leg_coords_mm,\
+    compute_target_bmr_heavy_weight_red_leg_coords_mm,\
+    get_red_leg_mount_holes_in_CS5_mm,CS5_to_PMA_inch
 
 inch2mm = 25.4
 
@@ -61,70 +63,67 @@ test-petal6-light-red-leg.yaml or test-petal6-heavy-red-leg.yaml
     measured_bmr_CS5_inch = np.zeros((3,number_of_balls))
 
     print("Input BMR coordinates (inch):")
-    valid_bmr = np.repeat(True,number_of_balls)
     for index,bmr_label in enumerate(bmr_labels) :
         if bmr_label in inputs :
             measured_bmr_CS5_inch[:,index] = str2array(inputs[bmr_label])
             print("{} {}".format(bmr_label,inputs[bmr_label]))
 
-    # align target bmr in z ?
-    # target_bmr_CS5_inch[2] = np.mean(target_bmr_CS5_inch[2])
+    xyz1=target_bmr_CS5_inch
 
-    xyz1=target_bmr_CS5_inch[:,valid_bmr]*inch2mm+0.
-    xyz2=measured_bmr_CS5_inch[:,valid_bmr]*inch2mm+0.
-    print("target   BMR mm z=",xyz1[2])
-    print("measured BMR mm z=",xyz2[2])
-    print("measured-target BMR dx (mm)=",np.mean(xyz2[0]-xyz1[0]))
-    print("measured-target BMR dy (mm)=",np.mean(xyz2[1]-xyz1[1]))
-    xyz2[2] += np.mean(xyz1[2]-xyz2[2])
+    # 'measured=solidworks' bmr
+    xyz2=measured_bmr_CS5_inch
 
-    # just ignore the offsets in z?
-    #xyz2[2] = xyz1[2]
+    # target holes
+    xyz3=get_red_leg_mount_holes_in_CS5_mm(petal)/inch2mm
 
-    cs5_invert_mm = Transfo()
-    rms=cs5_invert_mm.fit(xyz1,xyz2)
-    print("Inverse transfo fit rms=",rms,"mm")
-    print(cs5_invert_mm)
-
-    xyz = get_red_leg_mount_holes_in_CS5_mm(petal)
-    xyz2 = cs5_invert_mm.apply(xyz)
-    for b in range(xyz.shape[1]) :
-        print("before fit of bmr, hole",b,"xyz CS5 mm =",array2str(xyz[:,b]))
-    for b in range(xyz2.shape[1]) :
-        print("after fit of bmr, hole",b,"xyz CS5 mm =",array2str(xyz2[:,b]))
-
-    # the light weight solidworks mount hole xyz coords
-    xyz3=np.zeros((3,3))
-    xyz3[:,0]=[-323.19643922,340.10791362,-1759.2820881]
-    xyz3[:,1]=[-270.14129446,310.56739657,-1759.27470220]
-    xyz3[:,2]=[-219.92149856,364.62873842,-1759.1524087]
-    for b in range(xyz3.shape[1]) :
-        print("solidworks, hole",b,"xyz CS5 mm =",array2str(xyz3[:,b]))
-    for b in range(xyz3.shape[1]) :
-        print("delta, hole",b,"xyz CS5 mm =",array2str(xyz2[:,b]-xyz3[:,b]))
-
-    # the GS xyz coords
+    # 'measured=solidworks' holes
     xyz4=np.zeros((3,3))
-    xyz4[:,0]=[-324.24768586,338.65356890,-1759.14749184]
-    xyz4[:,1]=[-271.19422020,309.11004225,-1759.12740842]
-    xyz4[:,2]=[-220.97156843,363.16829061,-1758.87726430]
+    xyz4[:,0]=[-323.19643922,340.10791362,-1759.2820881]
+    xyz4[:,1]=[-270.14129446,310.56739657,-1759.27470220]
+    xyz4[:,2]=[-219.92149856,364.62873842,-1759.1524087]
+    xyz4 /= inch2mm
 
-    print("dx(light-GS)={:.4f} mm".format(np.mean(xyz3[0]-xyz4[0])))
-    print("dy(light-GS)={:.4f} mm".format(np.mean(xyz3[1]-xyz4[1])))
-    print("dz(light-GS)={:.4f} mm".format(np.mean(xyz3[2]-xyz4[2])))
+    if 1 :
+        # convert coords to PMA, then apply PMA corr
+        # after that the axes should be aligned ... ?
+        xyz1 = CS5_to_PMA_inch(xyz1) # target bmr
+        xyz2 = CS5_to_PMA_inch(xyz2) # 'measured=solidworks' bmr
+        xyz3 = CS5_to_PMA_inch(xyz3) # target holes
+        xyz4 = CS5_to_PMA_inch(xyz4) # 'measured=solidworks' holes
 
-    t = Transfo()
-    rms=t.fit(xyz,xyz3)
-    xyz5=t.apply(xyz)
-    print("t rms=",rms)
-    print("t dx=",np.mean(xyz5[0]-xyz3[0]))
-    print("t dy=",np.mean(xyz5[1]-xyz3[1]))
-    plt.figure("quiver")
-    plt.quiver(xyz3[0],xyz3[1],xyz5[0]-xyz3[0],xyz5[1]-xyz3[1])
-    #plt.quiver(xyz3[0],xyz3[1],xyz2[0]-xyz3[0],xyz2[1]-xyz3[1])
+    if 1 :
+        # apply transfo that has been fit to align
+        # the PMA (this aligns the z axes)
+        t=Transfo3D.read("transfo-petal6-light-red-leg.yaml")
+        print(t)
+        xyz1 = t.apply(xyz1)
+        xyz2 = t.apply(xyz2)
+        xyz3 = t.apply(xyz3)
+        xyz4 = t.apply(xyz4)
 
+    # now should be aligned
+    # discard z offsets for both pairs
+    xyz2[2] += np.mean(xyz1[2]-xyz2[2])
+    xyz4[2] += np.mean(xyz3[2]-xyz4[2])
 
-    plt.show()
+    # HACK
+    # xyz2[2] = xyz1[2]
+    # xyz4[2] = xyz3[2]
 
+    cs5_invert_mm = Transfo3D()
+    rms=cs5_invert_mm.fit(xyz1,xyz2)
+    print("Inverse transfo fit rms =",rms,"inch")
+    print("Inverse transfo fit mm  =",rms*inch2mm,"mm")
+    print(cs5_invert_mm)
+    xyz3b = cs5_invert_mm.apply(xyz3)
+    delta = xyz4-xyz3b
+    print("delta (inch) hole 1 =",delta[:,0])
+    print("delta (inch) hole 2 =",delta[:,1])
+    print("delta (inch) hole 3 =",delta[:,2])
+    print("delta (mm) hole 1 =",delta[:,0]*inch2mm)
+    print("delta (mm) hole 2 =",delta[:,1]*inch2mm)
+    print("delta (mm) hole 3 =",delta[:,2]*inch2mm)
+    max_dist_mm=np.max(np.sqrt(delta[0,:]**2+delta[1,:]**2)*inch2mm)
+    print("max dist (mm) =",max_dist_mm)
 if __name__ == '__main__':
     main()
